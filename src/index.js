ec21389e113687704920a8b7b4ba682d6c93f622
Alghazi, Bilal <bilal_ibrahim_alghazi@manulife.com>
2018-11-13 11:23:24 -0500

    Set default value for all of the component properties

--- src/index.js
+++ src/index.js
@@ -7,21 +7,31 @@ import {
   LanguageCode,
   CountryCode
 } from 'address-service';
-import { filter } from "lodash";
 
 class AddressLookup extends Component {
   
   constructor(props){
     super(props);
+    let defaultLaguage = LanguageCode.English;
+    if (this.props.language === "FRE"){
+      defaultLaguage = LanguageCode.French;
+    }
     this.state = {
       suggestions: [],
-      value: ""
+      value: "",
+      forceRenderSuggestions: false,
+      nestedAddressID: "",
+      language: defaultLaguage,
+      renderSource: ""
     };
   }
 
   renderSuggestion = (suggestion) => {
     return (
-        <div>{suggestion.addressText}</div>
+        <div>
+          <i style={{paddingRight: "5px"}} class="fas fa-map-marker-alt"></i>
+          {suggestion.addressText}
+        </div>
       );
   }
 
@@ -36,64 +46,89 @@ class AddressLookup extends Component {
   };
 
   onSuggestionsFetchRequested = async ({ value }) => {
+    // console.log("Fetching addresses", this.state.nestedAddressID);
     // Fetch value from Canada Post API
     const addressProvider = AddressServiceProvider.getAddressService(ServiceProvider.CanadaPostService)
-    const AddressSearchList = await addressProvider.findAddress({searchTerm: value, language: LanguageCode.English, country: CountryCode.Canada});
-
+    let AddressSearchList;
+    if (this.state.nestedAddressID === ""){
+      // New address search
+      AddressSearchList = await addressProvider.findAddress({searchTerm: value, language: this.state.defaultLaguage, country: CountryCode.Canada});
+    } else {
+      // Nested address search
+      AddressSearchList = await addressProvider.findNestedAddress({id: this.state.nestedAddressID, language: this.state.defaultLaguage, country: CountryCode.Canada});
+    }
+    
     this.setState({
-      suggestions: AddressSearchList
+      suggestions: AddressSearchList,
+      value: (this.state.nestedAddressID === "") ? this.state.value : AddressSearchList[0].addressText,
+      forceRenderSuggestions: (this.state.nestedAddressID === ""),
+      renderSource: "onSuggestionsFetchRequested"
     });
   };
 
-  escapeRegexCharacters = (str) => {
-    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  }
-
-  getSuggestions = (value) => {
-    const escapedValue = this.escapeRegexCharacters(value.trim());
-    
-    if (escapedValue === '') {
-      return [];
-    }
-  
-    const regex = new RegExp('^' + escapedValue, 'i');
-  
-    return languages.filter(language => regex.test(language.name));
-  }
-
   onChange = (event, { newValue, method }) => {
     this.setState({
-      value: newValue
+      nestedAddressID: "",
+      forceRenderSuggestions: false,
+      value: newValue,
+      renderSource: "onChange"
     });
   };
 
   onSuggestionSelected = async (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
+    // console.log("Address selected", suggestion.isComplete);
+    if(!suggestion.isComplete){
+      // Selected address is not a complete address
+      // Fetch the rest of the addresses
+      this.setState({
+        forceRenderSuggestions: true,
+        nestedAddressID: suggestion.id,
+        renderSource: "onSuggestionSelected"
+      });
+    }
     if (this.props.onAddressSelected){
-      // Get the selected address information from the API
       const addressProvider = AddressServiceProvider.getAddressService(ServiceProvider.CanadaPostService)
-      const AddressDetails = await addressProvider.findAddressDetails({id: suggestion.id, language: LanguageCode.English});
-      // Make English the default language
-      const selectedLanguage = this.props.language ? this.props.language : "ENG";
-      const FilteredAddressDetails = filter(AddressDetails, { language: selectedLanguage });
-      this.props.onAddressSelected(
-        FilteredAddressDetails[0].streetAddress1,
-        FilteredAddressDetails[0].city,
-        FilteredAddressDetails[0].postalCode,
-        FilteredAddressDetails[0].province,
-        FilteredAddressDetails[0].country,
-        FilteredAddressDetails[0].id,
-        method
-      );
+      if (suggestion.isComplete){
+        // Selected address is a completed address
+        // Get the selected address information from the API
+        const AddressDetails = await addressProvider.findAddressDetails({id: suggestion.id, language: this.state.defaultLaguage});
+        this.props.onAddressSelected(
+          AddressDetails[0].streetAddress1,
+          AddressDetails[0].city,
+          AddressDetails[0].postalCode,
+          AddressDetails[0].province,
+          AddressDetails[0].country,
+          AddressDetails[0].id,
+          method
+        );
+      }
     }
   }
 
   render() {
+    // console.log(this.state.renderSource);
     const inputProps = {
-      placeholder: this.props.inputPlaceholder,
-      value: this.props.value,
-      onChange: this.props.onChange,
+      placeholder: this.props.inputPlaceholder ? this.props.inputPlaceholder : "Enter an address...",
+      value: this.state.value,
+      onChange: this.onChange,
       onBlur: this.props.onBlur
     };
+    const defaultTheme = {
+      container:                'address-lookup__container',
+      containerOpen:            'address-lookup__container--open',
+      input:                    'address-lookup__input',
+      inputOpen:                'address-lookup__input--open',
+      inputFocused:             'address-lookup__input--focused',
+      suggestionsContainer:     'address-lookup__suggestions-container',
+      suggestionsContainerOpen: 'address-lookup__suggestions-container--open',
+      suggestionsList:          'address-lookup__suggestions-list',
+      suggestion:               'address-lookup__suggestion',
+      suggestionFirst:          'address-lookup__suggestion--first',
+      suggestionHighlighted:    'address-lookup__suggestion--highlighted',
+      sectionContainer:         'address-lookup__section-container',
+      sectionContainerFirst:    'address-lookup__section-container--first',
+      sectionTitle:             'address-lookup__section-title'
+    }
     return (
       <div>
         <Autosuggest 
@@ -108,9 +143,10 @@ class AddressLookup extends Component {
           renderInputComponent={this.props.renderInputComponent}
           focusInputOnSuggestionClick={this.props.focusInputOnSuggestionClick}
           highlightFirstSuggestion={this.props.highlightFirstSuggestion}
-          alwaysRenderSuggestions={this.props.alwaysRenderSuggestions}
+          alwaysRenderSuggestions={this.props.alwaysRenderSuggestions || this.state.forceRenderSuggestions}
+          focusInputOnSuggestionClick={true}
           onSuggestionHighlighted={this.props.onAddressHighlighted}
-          theme={this.props.theme} />
+          theme={this.props.theme ? this.props.theme : defaultTheme} />
       </div>
     );
   }
